06c903b42b977df5e9e45288ffaa165095105816
Samuel Weiser <samuel.weiser@iaik.tugraz.at>
2019-12-06 17:43:04 +0100

    v1.2 released

--- datagui/package/ui/ZoomTabView.py
+++ datagui/package/ui/ZoomTabView.py
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-@version 1.1
+@version 1.2
 
 """
 
